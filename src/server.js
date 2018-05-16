018932a4002df3811e3ea330203e1492251b2b86
James Digrazia <jtd2401@rit.edu>
2018-05-16 13:45:43 -0400

    Setup initial HTTP Server and print out request body for testing

---
+++ src/server.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const url = require('url');
+const query = require('querystring');
+
+const responses = require('./responses.js');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const onRequest = (request, response) => {
+  console.log(request.url);
+  console.log(request.body);
+
+  const parsedUrl = url.parse(request.url);
+  const params = query.parse(parsedUrl.query);
+  const path = parsedUrl.pathname;
+};
+
+http.createServer(onRequest).listen(port);
+console.log("Listening on 127.0.0.1: " + port);
